b76f8849020dda7ea157c2d8117d5954a98d5f74
Billy Joseph <ingbillyjoseph@gmail.com>
2019-04-17 15:34:20 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -14,6 +14,6 @@ def get_the_keys(groceries)
 #  dairy: ["milk", "yogurt", "cheese"],
 #  vegetable: ["carrots", "broccoli", "cucumbers"],
 #  meat: ["chicken", "steak", "salmon"],
-#  grains: ["rice", "pasta"]
-# }
+  grains: ["rice", "pasta"]
+ }
 end
